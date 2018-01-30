c036b5f38d7d138516edbb6ff58694ba1d7f54ba
DUKANG <dk1022lt@163.com>
2018-01-30 19:14:15 +0800

    mybatis的逆向工程

---
+++ src/com/dukang/generator/GeneratorAll.java
@@ -0,0 +1,43 @@
+package com.dukang.generator;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author DUKANG
+ * @create 2018/01/25
+ * @since 1.0.0
+ * Description:
+ */
+public class GeneratorAll {
+
+    public void generator() throws Exception{
+
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        //指定 逆向工程配置文件
+        File configFile = new File("generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+                callback, warnings);
+        myBatisGenerator.generate(null);
+
+    }
+
+    public static void main(String[] args) {
+        GeneratorAll generatorAll = new GeneratorAll();
+        try {
+            generatorAll.generator();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
